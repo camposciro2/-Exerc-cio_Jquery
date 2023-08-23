5ffc057a4047ee269ece348361d55e6adc6fc436
camposciro2 <campos.ciro@outlook.com>
2023-08-23 13:15:14 -0300

    Atualização css e X para retirada de tarefa

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
     $("#taskForm").submit(function (event) {
         event.preventDefault();
-        const taskName = $("#taskInput").val();
+        const taskName = $("#taskInput").val().trim();  // trim para remover espaços em branco
+
         if (taskName) {
             const taskItem = $("<li></li>").text(taskName);
-            taskItem.click(function () {
-                $(this).toggleClass("completed");
+
+            const deleteBtn = $("<button class='delete-btn'>X</button>");
+            deleteBtn.click(function () {
+                $(this).parent().remove();
             });
+
+            taskItem.append(deleteBtn);
+
+            taskItem.click(function (e) {
+                if (!$(e.target).hasClass('delete-btn')) {
+                    $(this).toggleClass("completed");
+                }
+            });
+
             $("#taskList").append(taskItem);
             $("#taskInput").val('');
+        } else {
+            alert("Por favor, insira uma tarefa válida.");
         }
     });
 });
